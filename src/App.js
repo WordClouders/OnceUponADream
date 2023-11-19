3eec401be49d3f4a8905760a9acfa7a33732fdab
Nithin Muthukumar <kathrikai30201094@gmail.com>
2023-11-19 09:37:06 -0500

    adding hashconnect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Navibar from './Features/Navibar';
-import './App.css';
+import Navibar from "./Features/Navibar";
+import "./App.css";
+import React, { useState } from "react";
+import { pairHashpack, contractSigningFunc } from "./hashconnect";
 
 function App() {
+  const [pairingString, setPairingString] = useState("");
+  // <p>{pairingString}</p>
+
   return (
     <main>
       <Navibar></Navibar>
+      <button
+        onClick={async () => {
+          const saveData = await pairHashpack();
+          setPairingString(saveData.pairingString);
+        }}
+      >
+        Pair wallet
+      </button>
+      <button onClick={contractSigningFunc}>Pair wallet</button>
     </main>
   );
 }
